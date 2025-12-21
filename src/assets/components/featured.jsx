d47dbb6a4dab0e02b2196110a7e7c338e0569175
alex553478 <alexmarian5534@gmail.com>
2025-12-21 20:19:19 +0200

    featured card

--- src/assets/components/featured.jsx
+++ src/assets/components/featured.jsx
@@ -1,23 +1,74 @@
-import React from 'react'
-import './featured.css'
+import React from "react";
+import "./featured.css";
 
-const featured = () => {
+const Featured = () => {
   return (
-    <div>
-       <div className="featured">
-        <div className="featuredItem">
-            <img src= "" alt="ford focus" className="featuredImg" />
-            <div className="featuredTitles">
-                <h1>Ford Focus MK3</h1>
-                <p>Price at 7,500Eur</p>
-                <p>2017 model</p>
-                <p>200,000 km</p>
-                <p>Engime : 1,6TDCI 95HP</p>
-            </div>
-        </div>
-       </div>
-    </div>
-  )
-}
+    <div className="featured">
+      <h1>Favorite Cars</h1>
 
-export default featured
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1503376780353-7e6692767b70"
+          alt="Porsche"
+          className="featuredImg"
+        />
+
+        <div className="featuredTitles">
+          <h2>Porsche</h2>
+          <p>€72,000 • 2021 • 15.000 km</p>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <img
+          src="https://images.pexels.com/photos/358070/pexels-photo-358070.jpeg"
+          alt="Viper"
+          className="featuredImg"
+        />
+
+        <div className="featuredTitles">
+          <h2>Viper</h2>
+          <p>€45,000 • 2020 • 32.000 km</p>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg"
+          alt="Lotus"
+          className="featuredImg"
+        />
+
+        <div className="featuredTitles">
+          <h2>Lotus</h2>
+          <p>€60,000 • 2005 • 25.000 km</p>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://images.pexels.com/photos/2127039/pexels-photo-2127039.jpeg"
+          alt="BMW M3"
+          className="featuredImg"
+        />
+
+        <div className="featuredTitles">
+          <h2>BMW M3</h2>
+          <p>€100,000 • 2020 • 25.000 km</p>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://images.pexels.com/photos/1035108/pexels-photo-1035108.jpeg"
+          alt="Audi A5 Break"
+          className="featuredImg"
+        />
+
+        <div className="featuredTitles">
+          <h2>Audi A5 Break S</h2>
+          <p>€50,000 • 2015 • 70.000 km</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
