37b6cb4bb7cb5a6c3c00d6a36556c513c95e370a
alex553478 <alexmarian5534@gmail.com>
2025-12-21 22:05:58 +0200

    banner-ok

--- src/assets/components/featured.jsx
+++ src/assets/components/featured.jsx
@@ -1,73 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./featured.css";
 
 const Featured = () => {
+  const cars = [
+    {
+      name: "Porsche",
+      info: "€72,000 • 2021 • 15.000 km",
+      img: "https://images.unsplash.com/photo-1503376780353-7e6692767b70",
+    },
+    {
+      name: "Viper",
+      info: "€45,000 • 2020 • 32.000 km",
+      img: "https://images.pexels.com/photos/358070/pexels-photo-358070.jpeg",
+    },
+    {
+      name: "Lotus",
+      info: "€60,000 • 2005 • 25.000 km",
+      img: "https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg",
+    },
+    {
+      name: "BMW M3",
+      info: "€100,000 • 2020 • 25.000 km",
+      img: "https://images.pexels.com/photos/2127039/pexels-photo-2127039.jpeg",
+    },
+    {
+      name: "Audi A5 Break S",
+      info: "€50,000 • 2015 • 70.000 km",
+      img: "https://images.pexels.com/photos/1035108/pexels-photo-1035108.jpeg",
+    },
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  // auto slider
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
+  const nextSlide = () => {
+    setIndex((prev) => (prev + 1) % cars.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((prev) => (prev - 1 + cars.length) % cars.length);
+  };
+
   return (
-    <div className="featured">
+    <section className="featured">
       <h1>Favorite Cars</h1>
 
       <div className="featuredItem">
         <img
-          src="https://images.unsplash.com/photo-1503376780353-7e6692767b70"
-          alt="Porsche"
+          src={cars[index].img}
+          alt={cars[index].name}
           className="featuredImg"
         />
 
         <div className="featuredTitles">
-          <h2>Porsche</h2>
-          <p>€72,000 • 2021 • 15.000 km</p>
+          <h2>{cars[index].name}</h2>
+          <p>{cars[index].info}</p>
         </div>
-      </div>
 
-      <div className="featuredItem">
-        <img
-          src="https://images.pexels.com/photos/358070/pexels-photo-358070.jpeg"
-          alt="Viper"
-          className="featuredImg"
-        />
-
-        <div className="featuredTitles">
-          <h2>Viper</h2>
-          <p>€45,000 • 2020 • 32.000 km</p>
-        </div>
+        {/* BUTOANE */}
+        <button className="navBtn left" onClick={prevSlide}>‹</button>
+        <button className="navBtn right" onClick={nextSlide}>›</button>
       </div>
-      <div className="featuredItem">
-        <img
-          src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg"
-          alt="Lotus"
-          className="featuredImg"
-        />
-
-        <div className="featuredTitles">
-          <h2>Lotus</h2>
-          <p>€60,000 • 2005 • 25.000 km</p>
-        </div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://images.pexels.com/photos/2127039/pexels-photo-2127039.jpeg"
-          alt="BMW M3"
-          className="featuredImg"
-        />
-
-        <div className="featuredTitles">
-          <h2>BMW M3</h2>
-          <p>€100,000 • 2020 • 25.000 km</p>
-        </div>
-      </div>
-      <div className="featuredItem">
-        <img
-          src="https://images.pexels.com/photos/1035108/pexels-photo-1035108.jpeg"
-          alt="Audi A5 Break"
-          className="featuredImg"
-        />
-
-        <div className="featuredTitles">
-          <h2>Audi A5 Break S</h2>
-          <p>€50,000 • 2015 • 70.000 km</p>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
