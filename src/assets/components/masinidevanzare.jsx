899b38d2fa0d6d05cc8866455afea152f2692599
alex553478 <alexmarian5534@gmail.com>
2026-01-04 20:59:22 +0200

    full page masini

--- src/assets/components/masinidevanzare.jsx
+++ src/assets/components/masinidevanzare.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
+import "./masini.css";
 
-const masinidevanzare = () => {
+const Masini = () => {
   const cars = [
     {
       name: "Porsche",
@@ -26,8 +27,70 @@ const masinidevanzare = () => {
       name: "Audi A5 Break S",
       info: "€50,000 • 2015 • 70.000 km",
       img: "https://images.pexels.com/photos/1035108/pexels-photo-1035108.jpeg",
+
+      img:"https://i.postimg.cc/J4zk9ksH/pexels-billingphotography-9695287.jpg",
+      name: "Skoda Fabia",
+      info: "$10,000 -2021 - 120.000 km",
+      
     },
+    {img:"https://i.postimg.cc/TwTwgzmK/pexels-mikebirdy-452099.jpg",
+      name: "Bmw Seria 3",
+      info: "$4,000 -2008 - 200.000 km",
+    },
+    {
+      img:"https://i.postimg.cc/zGJ8dC3m/222305-4527987.webp",
+      name: "Dacia Duster 1",
+      info: "$5,000 - 2011 - 150.000 km 4x4"
+    },
+    {
+      img:"https://i.postimg.cc/3RbkhL4K/car-photo-411691.jpg",
+      name: "Ford Focus mk3",
+      info: "$5,000 - 2011 - 190.000 km",
+    },
+    {
+      img:"https://i.postimg.cc/m2VrCLyz/skoda-octavia-iii-scout.jpg",
+      name: "Skoda Octavia III Scout",
+      info: "$11,000 - 2016 - 200.000 km",
+
+    },
+    {
+      img:"https://i.postimg.cc/nc83tZ8c/7b2653d1-12f1-40a5-92aa-23e0b33bfc0f-vw-sharan-2-0-tdi-dsg-bluemotion-technology-highline.jpg",
+      name: "VW Sharan 2.0 TDI",
+      info: "$5,000 - 2014 - 150.000 km",
+
+    },
+    {img:"https://i.postimg.cc/vHqDV8Q8/720x540.webp",
+      name: "VW Golf 7 R line",
+      info: "$24.000 - 2019 - 70.000 km"
+    },
+    {
+      img:"https://i.postimg.cc/4N6kDBSv/b8a077ca-b717-4561-9ba7-3c365a8bd093-rule-mo-1600.jpg",
+      name: "Honda Civic 1.8 vtec",
+      info: "$4,570 - 2008 - 200.000 km",
+    },
+    {
+      img:"https://i.postimg.cc/L50Y2mXW/Logan-driving-front-jpg-auto-format-cs-tinysrgb-fit-clip-ixlib-rb-1-1.jpg",
+      name: "Renault Logan MCV 2.0 DCI 4X4",
+      info: "$6,000 - 2012 - 100.000 km",
+    },
+    {
+      img:"https://i.postimg.cc/RFMCbq9B/test-de-fi-62519.jpg",
+      name: "Dacia Logan 2 Stepway",
+      info: "$7,000 - 2015 - 200.000 km",
+    },
+    {
+      img:"https://i.postimg.cc/gkvzdvJP/image-s-1440x0-q-100.jpg",
+      name: "Dacia Sandero Stepway 1.5 Dci",
+      info: "$11,000 - 2019 - 1000.000 km"
+    },
+    {
+      img:"https://i.postimg.cc/TYP0vcQq/39b4d027-0b5d-45f0-b8b4-748e16ddc73a-rule-mo-1600.jpg",
+      name: "Mazda 6",
+      info: "$15,000 - 2015 - 200.000 km",
+    }
+    
   ];
+
   return (
     <div className="masini-page">
       <h1>🚗 Mașini de vânzare</h1>
@@ -45,8 +108,7 @@ const masinidevanzare = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default masinidevanzare
-
+export default Masini;
